c4d6cbcc8aeb7d9925a3b4a6a4a4f53efce0f350
Viacheslav Zhuravskyi <zhuravsky_v@a.krok.edu.ua>
2019-02-07 21:29:17 +0200

    feat: implement PhotosService

---
+++ src/da-layer/services/PhotosService.ts
@@ -0,0 +1,15 @@
+import { IPhotosService } from '../abstract/IPhotosService';
+import { endpoint } from '../apiEndpoint';
+import { Photos } from '../models/Photos';
+
+class PhotosService implements IPhotosService {
+  public getPhotos(offset: number, count: number): Promise<Photos> {
+    return new Promise((resolve, reject) => {
+      fetch(endpoint)
+        .then(res => {
+          resolve((res as unknown) as Photos);
+        })
+        .catch(error => reject(error));
+    });
+  }
+}
