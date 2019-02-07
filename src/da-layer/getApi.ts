855ced8b8cf57ab2b56d0970c1e3a8eee9bc7fd3
Viacheslav Zhuravskyi <zhuravsky_v@a.krok.edu.ua>
2019-02-07 21:39:38 +0200

    docs: document getApi method

--- src/da-layer/getApi.ts
+++ src/da-layer/getApi.ts
@@ -3,4 +3,9 @@ import { ApiProvider } from './provider/ApiProvider';
 
 const provider: IApiProvider = new ApiProvider();
 
+/**
+ * Provides ApiProvider methods to store layer
+ * 
+ * e.g. getApi().photos.getPhotos(...)...
+ */
 export const getApi = (): IApiProvider => provider;
