f3082eb28393e66f8eee86dae237ca011cde56a2
Viacheslav Zhuravskyi <zhuravsky_v@a.krok.edu.ua>
2019-02-10 18:38:22 +0200

    fix: getPhotos method

--- src/da-layer/services/PhotosService.ts
+++ src/da-layer/services/PhotosService.ts
@@ -5,7 +5,7 @@ import { Photos } from '../models/Photos';
 export class PhotosService implements IPhotosService {
   public getPhotos(offset: number, count: number): Promise<Photos> {
     return new Promise((resolve, reject) => {
-      fetch(endpoint)
+      fetch(endpoint + `/photos?offset=${offset}&count=${count}`)
         .then(res => {
           res.json().then(data => {
             resolve(data as Photos);
