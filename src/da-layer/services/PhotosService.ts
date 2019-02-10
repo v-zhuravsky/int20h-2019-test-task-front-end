0a7e661faf2d5ca7fd13260c8f752beb82df3466
Viacheslav Zhuravskyi <zhuravsky_v@a.krok.edu.ua>
2019-02-10 21:10:22 +0200

    feat: add emotion filter to fetch photos request

--- src/da-layer/services/PhotosService.ts
+++ src/da-layer/services/PhotosService.ts
@@ -3,9 +3,15 @@ import { endpoint } from '../apiEndpoint';
 import { Photos } from '../models/Photos';
 
 export class PhotosService implements IPhotosService {
-  public getPhotos(offset: number, count: number): Promise<Photos> {
+  public getPhotos(
+    offset: number,
+    count: number,
+    emotion: string
+  ): Promise<Photos> {
     return new Promise((resolve, reject) => {
-      fetch(endpoint + `/photos?offset=${offset}&count=${count}`)
+      fetch(
+        endpoint + `/photos?offset=${offset}&count=${count}&emotion=${emotion}`
+      )
         .then(res => {
           res.json().then(data => {
             resolve(data as Photos);
