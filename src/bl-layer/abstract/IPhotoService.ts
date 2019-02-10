ca548366348fc3844d67d1dc359c8f2274898a24
Igor Onyshchenko <optimum.flaminis@gmail.com>
2019-02-10 22:40:06 +0200

    feat: implement bl photos service

---
+++ src/bl-layer/abstract/IPhotoService.ts
@@ -0,0 +1,6 @@
+import { Photos } from "../../da-layer/models/Photos";
+
+export interface IPhotosService {
+  getPhotos(page: number, emotion: string): Promise<Photos>
+  getPhotoUrl(photoUrl: string): string
+}
---
+++ src/bl-layer/services/PhotosService.ts
@@ -0,0 +1,21 @@
+import { IPhotosService } from '../abstract/IPhotoService';
+import { Photos } from '../../da-layer/models/Photos';
+import { getApi } from '../../da-layer/getApi';
+
+const itemsOnPage = 300;
+
+export class PhotosService implements IPhotosService {
+  api = getApi();
+
+  public getPhotos(page: number, emotion?: string): Promise<Photos> {
+    return this.api.photos.getPhotos(itemsOnPage, page * itemsOnPage);
+  }
+
+  public getPhotoUrl(url: string): string {
+    const arr = url.split('.');
+    const name = arr[arr.length - 2];
+    const newName = name.substr(0, name.length - 1) + 'z'
+    arr[arr.length - 2] = newName;
+    return arr.join('.');
+  }
+}
